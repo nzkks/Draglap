d16194937259f58b982fc7cdb098c668ad1a0329
nzkks <kkshanthosh@gmail.com>
2017-01-16 23:27:30 +1300

    Added draggable Angular directive

--- script.js
+++ script.js
@@ -2,4 +2,30 @@
   'use strict';
   var crownModule = angular.module('crown', []);
 
+  crownModule.directive('draggable', function($document) {
+    return function(scope, element, attr) {
+      var startX = 0, startY = 0, x = 0, y = 0;
+      element.on('mousedown', function(e) {
+        e.preventDefault();
+        startX = e.screenX - x;
+        startY = e.screenY - y;
+        $document.on('mousemove', mousemove);
+        $document.on('mouseup', mouseup);
+      });
+
+      function mousemove(e) {
+        y = e.screenY - startY;
+        x = e.screenX - startX;
+        element.css({
+          top: y + 'px',
+          left:  x + 'px'
+        });
+      }
+
+      function mouseup() {
+        $document.off('mousemove', mousemove);
+        $document.off('mouseup', mouseup);
+      }
+    };
+  });
 })(window.angular);
